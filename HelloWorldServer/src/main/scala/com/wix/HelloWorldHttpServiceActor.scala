a2109911d8be669a1fa2eeed61430d8c55390d34
maximri <maximri@wix.com>
2016-01-10 21:09:52 +0200

    Adding daytime-greeting endpoint to the server that should return a greeting according to the machine time

--- HelloWorldServer/src/main/scala/com/wix/HelloWorldHttpServiceActor.scala
+++ HelloWorldServer/src/main/scala/com/wix/HelloWorldHttpServiceActor.scala
@@ -1,11 +1,27 @@
 package com.wix
 
-
+import spray.http.DateTime
 import spray.routing.HttpServiceActor
 
-class HelloWorldHttpServiceActor extends HttpServiceActor {
+class HelloWorldHttpServiceActor(timeTeller: TimeTeller = new DateTimeNowTeller) extends HttpServiceActor {
 
   def receive = runRoute {
-    (get & path("hello")) { complete("Hello World!") }
+    (get & path("hello")) { complete("Hello World!") } ~
+      (get & path("daytime-greeting")) { complete(greeting) }
   }
+
+  private def greeting: String = {
+    timeTeller.whatsTheTime().hour match {
+      case h if h >= 6 && h <= 10 => "Good Morning"
+      case _ => "Good Day"
+    }
+  }
+}
+
+trait TimeTeller {
+  def whatsTheTime(): DateTime
+}
+
+class DateTimeNowTeller extends TimeTeller {
+  override def whatsTheTime(): DateTime = DateTime.now
 }
--- HelloWorldServer/src/test/scala/com/wix/HelloWorldServerTest.scala
+++ HelloWorldServer/src/test/scala/com/wix/HelloWorldServerTest.scala
@@ -1,37 +1,75 @@
 package com.wix
 
-import akka.actor.{ActorRef, ActorSystem, Props}
-import org.specs2.mutable.{Before, SpecificationWithJUnit}
+import akka.actor.{ActorSystem, Props}
+import org.specs2.mutable.{BeforeAfter, SpecificationWithJUnit}
 import org.specs2.specification.Scope
 import spray.client.pipelining._
-import spray.http.{HttpRequest, HttpResponse}
+import spray.http.{DateTime, HttpRequest, HttpResponse}
 import spray.routing.SimpleRoutingApp
 
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
 class HelloWorldServerTest extends SpecificationWithJUnit {
-  "HelloWorldServer" should {
+  sequential
 
+  "HelloWorldServer" should {
     "return 'hello world!' when sending a get request to '/hello' path" in new ctx {
       private val response: HttpResponse = get("/hello")
       response.entity.asString mustEqual "Hello World!"
     }
+
+    "return day time greeting suitable for day time on '/daytime-greeting' path" in new ctx {
+      override def before: Unit = {
+        val dayTimeTeller = constantTimeTeller(hour = 16)
+        setUpServer(Some(dayTimeTeller))
+      }
+
+      private val response: HttpResponse = get("/daytime-greeting")
+      response.entity.asString mustEqual "Good Day"
+    }
+
+    "return day time greeting suitable for morning time on '/daytime-greeting' path" in new ctx {
+      override def before: Unit = {
+        val morningTimeTeller = constantTimeTeller(hour = 9)
+        setUpServer(Some(morningTimeTeller))
+      }
+
+      private val response: HttpResponse = get("/daytime-greeting")
+      response.entity.asString mustEqual "Good Morning"
+    }
   }
 
-  trait ctx extends Scope with Before with SimpleRoutingApp {
+  trait ctx extends Scope with SimpleRoutingApp with BeforeAfter {
     lazy val port = 8060
     implicit val system = ActorSystem("TestEnv")
     // execution context for futures
+
     import system.dispatcher
 
-    override def before: Unit = {
-
-      lazy val helloWorldHttpServiceActor: ActorRef = system.actorOf(Props[HelloWorldHttpServiceActor])
+    def setUpServer(maybeTimeTeller: Option[TimeTeller] = None) = {
+      lazy val helloWorldHttpServiceActor =
+        system.actorOf(Props(
+          maybeTimeTeller.fold
+          (new HelloWorldHttpServiceActor())
+          (timeTeller => new HelloWorldHttpServiceActor(timeTeller))))
 
       startServer(interface = "localhost", port = port) {
         { ctx => helloWorldHttpServiceActor ! ctx }
       }
+    }
+
+    def constantTimeTeller(hour: Int) = new TimeTeller {
+      override def whatsTheTime(): DateTime = DateTime.now.copy(hour = hour)
+    }
+
+    override def before: Unit = {
+      setUpServer()
+    }
+
+    override def after: Unit = {
+      // Test code smell -> shouldn't be tested at this level
+      system.shutdown()
     }
 
     private val pipeline: HttpRequest => Future[HttpResponse] = sendReceive
